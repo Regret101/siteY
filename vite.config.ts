0b2e2424852e6ed7ff3357c9b963e27dcafd81d1
Regret101 <d1macom@yandex.ru>
2026-06-02 12:42:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/siteSmol/',
+    base: '/siteY/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
